d1921ef1ddd19ea300f015aecdc39c2230639c65
Scott Norvell <scottlnorvell@gmail.com>
2013-10-13 17:21:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundOcean::Application.config.secret_token = 'c3fecdbea4ae70d21214e5b3fd5d7865cdc74d5c0bb80dbf278d80c1711f1b36c744c4e7c89801f1a3065ff71c22530d37ae55c26aad05d7fcbf86d7beffcd8d'
