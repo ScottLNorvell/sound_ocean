9597b5af1c61a2e3dde0c009ceb437cf10c3fafc
Scott Norvell <scottlnorvell@gmail.com>
2013-10-14 11:11:05 -0400

    write basic model tests

---
+++ spec/models/playlist_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe Playlist do
+	
+	before { @playlist = Playlist.create name: 'test playlist', user_id: 2 }
+
+	subject { @playlist }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:user_id) }  
+
+	it { should be_valid } 
+
+end
---
+++ spec/models/song_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe Song do
+	
+	before { @song = Song.create title: 'Toxic', artist: 'Britney Spears', sc_track_id: 115301957 }
+
+	subject { @song }
+
+	it { should respond_to(:title) }
+	it { should respond_to(:artist) } 
+	it { should respond_to(:sc_track_id) }
+	it { should respond_to(:discoverer_id) } 
+	it { should respond_to(:discoveries) }   
+
+	it { should be_valid } 
+
+end
