78e56f2e4fdd108723c98cf942b76c04897c9f8c
davidhan527 <davidhan527@gmail.com>
2013-10-14 16:29:24 -0400

    changing new.html.erb file

--- db/migrate/20131014195237_create_users.rb
+++ db/migrate/20131014195237_create_users.rb
@@ -1,10 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password
-      t.string :password_confirmation
+      t.float :score
 
       t.timestamps
     end
