edf74dd0924e8cc10399f41cfbad40be7b6a4b88
francescawedemeyer <flwedemeyer@gmail.com>
2013-10-14 20:28:32 -0400

    Omniauth magic and devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014202317) do
+ActiveRecord::Schema.define(:version => 20131014223953) do
 
   create_table "playlists", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(:version => 20131014202317) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "username"
+    t.string   "provider"
+    t.integer  "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
