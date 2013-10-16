5cc4f7f7aa89d892e5fdb2cf89631a4bd9d03bb9
davidhan527 <davidhan527@gmail.com>
2013-10-16 11:17:12 -0400

    adding sidr

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,14 @@
 class SongsController < ApplicationController
   
   def discover
+    
     if song = Song.find_by_sc_track_id(params[:sc_track_id])
       song.discoveries.increment
     else
       song = current_user.discoveries.create(params[:song])
-    end
 
+    end
+    current_user.playlists << song
     render :json => song
   end
 
